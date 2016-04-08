da27c41b7ce820f6e3f4cecfc96c112ae933bd8a
Charles Fisher <chasjfish@gmail.com>
2016-04-08 14:16:10 -0500

    Path removal of files sent from IE.

--- xmit.c
+++ xmit.c
@@ -4,17 +4,17 @@
 #include <unistd.h>   /* Copyright 2015 Charles Fisher. Distributed    */
 #include <time.h>     /* under the terms of the GNU Lesser General     */
 #include <sys/stat.h> /* Public License (LGPL 2.1)                     */
-#include <sys/types.h>/* $Revision: 1.2 $ */
-#include "ccgi.h"     /* Compile with:
+#include <sys/types.h>/* $Revision: 1.3 $ Compile with:
 cc -Wall -I. -O2 -o xmit.cgi xmit.c ccgi.c strlcpy.c strlcat.c sha256.c
 http://www.slideshare.net/phdays/chw00t-breaking-unices-chroot-solutions
 BEWARE above, and: https://matt.sh/howto-c -- also consider -static    */
 
+#include "ccgi.h"    
 #define UPL_PATH "/upload/" /* Trailing slash/filename prefix perm 400 */
 #define TMP_PATH "/upload/cgi-upload" /* Must lie in same filesystem.  */
 #define LOG_PATH "/upload/upload.log" /* Must exist and be writable.   */
-#define uchar unsigned char /* 8-bit byte                              */
-#define uint unsigned int /*  32-bit word                              */
+#define uchar unsigned char /*  8-bit byte                             */
+#define uint unsigned int /*   32-bit word                             */
 typedef struct { uchar data[64]; uint datalen; uint bitlen[2];
  uint state[8]; } SHA256_CTX;
 void sha256_init(SHA256_CTX *);
@@ -25,7 +25,7 @@ size_t strlcpy(char *, const char *, size_t);
 
 int main(int argc, char **argv)
 {CGI_varlist *vl; int tlen=strlen(TMP_PATH); FILE *log; const char *name;
- char prefix[BUFSIZ] = UPL_PATH, dst[BUFSIZ], *p = getenv("SCRIPT_NAME");
+ char prefix[BUFSIZ]=UPL_PATH, dst[BUFSIZ], *p=getenv("SCRIPT_NAME"), *q;
 
  umask(umask((mode_t)0)|S_IWUSR|S_IWGRP|S_IWOTH|S_IXUSR|S_IXGRP|S_IXOTH);
 
@@ -54,29 +54,29 @@ int main(int argc, char **argv)
   if(!(val = CGI_lookup_all(vl, 0))) continue;
   for(i = 0; val[i]; i++) /* Does filename match TMP_PATH, and exist?  */
    if(!strncmp(val[i], TMP_PATH, tlen) && !stat(val[i], &junk_buf))
-   {time_t epoch = time(NULL); struct tm *rightnow = localtime(&epoch);
+   {time_t epoch = time(NULL); struct tm *now = localtime(&epoch);
 
     j = i++; /* Now, val[j] == tmp_name, val[i] == user's sent name.   */
-    strftime(dst, BUFSIZ, "%y/%m/%d %H:%M:%S", rightnow);
+    strftime(dst, BUFSIZ, "%y/%m/%d %H:%M:%S", now);
     fprintf(log, "%s %s %s", dst, getenv("REMOTE_ADDR"), val[i]);
 
-    if(!strlen(val[i]) || strchr(val[i], '/') || strchr(val[i], '\\') ||
-     strlcpy(dst, prefix, BUFSIZ) >= BUFSIZ || /* Skip if empty, over- */
-     strlcat(dst, val[i], BUFSIZ) >= BUFSIZ)   /*  size, or malicious. */
+    if((p = strrchr(val[i], '/')) != NULL) p++; else p = val[i];
+    if((q = strrchr(p, '\\')) != NULL) p = q + 1; /*IE sends full path.*/
+
+    if(strlcpy(dst, prefix, BUFSIZ) >= BUFSIZ ||
+     strlcat(dst, p, BUFSIZ) >= BUFSIZ) /* Skip if basename oversized. */
     {
-     printf("error\n"); fprintf(log, " _ABUSE-RETAINED_ %s\n", val[j]);
+     printf("error\n"); fprintf(log, " _FLEN-RETAINED_ %s\n", val[j]);
      continue;
     }
 
-    if(link(val[j], dst))
-    { /* On link failure, try our best to keep this data.              */
-     if(strlcat(dst, val[j] + tlen, BUFSIZ) >= BUFSIZ ||
-      link(val[j], dst)) /* mkstemp suffix appended to filename.       */
-     {
-      printf("name_error\t%s\n", val[i]);
-      fprintf(log, " _LINK-RETAINED_ %s\n", val[j]); continue;
-     } else fprintf(log, " _RENAMED_ %s", dst);
-    }
+    if(link(val[j], dst) && /* On link failure, try to keep this data. */
+     (strlcat(dst, val[j] + tlen, BUFSIZ) >= BUFSIZ ||
+     link(val[j], dst))) /* mkstemp suffix appended to filename.       */
+    {
+     printf("name_error\t%s\n", val[i]);
+     fprintf(log, " _LINK-RETAINED_ %s\n", val[j]); continue;
+    } else fprintf(log, " _RENAMED_ %s", dst);
 
     if(unlink(val[j]))
     {
